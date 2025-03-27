3e8f10087566ce7a8dafaf3eca8bb2e2bebdfddd
Muhammad A Faishal <maafaishal@gmail.com>
2025-03-27 14:25:43 +0700

    build: vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [react(), tailwindcss(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
